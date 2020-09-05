2927a2ecb03570766f618ef272056d6b50875fa6
Elias Roberts <elias@eroberts.dev>
2020-09-05 12:23:39 -0600

    fixed parallax scrolling for all cards

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,18 +1,18 @@
 const carousel = document.querySelectorAll('.carousel');
 for (let post of carousel) {
-	var flkty = new Flickity(post, {
+	let flkty = new Flickity(post, {
 		imagesLoaded: true,
 		percentPosition: false
 	});
-	var imgs = post.querySelectorAll('.carousel-cell img');
-	var docStyle = document.documentElement.style;
-	var transformProp = typeof docStyle.transform == 'string' ? 'transform' : 'WebkitTransform';
-}
+	let imgs = post.querySelectorAll('.carousel-cell img');
+	let docStyle = document.documentElement.style;
+	let transformProp = typeof docStyle.transform == 'string' ? 'transform' : 'WebkitTransform';
 
-flkty.on('scroll', function() {
-	flkty.slides.forEach(function(slide, i) {
-		var img = imgs[i];
-		var x = (slide.target + flkty.x) * -1 / 3;
-		img.style[transformProp] = 'translateX(' + x + 'px)';
+	flkty.on('scroll', function() {
+		flkty.slides.forEach(function(slide, i) {
+			let img = imgs[i];
+			let x = (slide.target + flkty.x) * -1 / 3;
+			img.style[transformProp] = 'translateX(' + x + 'px)';
+		});
 	});
-});
+}
