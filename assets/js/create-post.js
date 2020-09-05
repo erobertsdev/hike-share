33985747ec65743acfc7279d37a6aee538238de3
Elias Roberts <elias@eroberts.dev>
2020-09-05 15:40:10 -0600

    some styling

--- assets/js/create-post.js
+++ assets/js/create-post.js
@@ -1,1 +1,21 @@
 import { key } from './key.js';
+
+const search = async () => {
+	try {
+		const response = await axios.get('https://www.hikingproject.com/data/get-trails?', {
+			params: {
+				key: key,
+				lat: lat,
+				lon: lon,
+				maxDistance: 200,
+				maxResults: 20
+			}
+		});
+
+		console.log(response.data);
+		renderTrails(response.data, 'current');
+	} catch (error) {
+		console.log(error);
+		return;
+	}
+};
