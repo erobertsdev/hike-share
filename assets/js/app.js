485b9d25c69e379135f3bca99bb6f0bc38d11a08
Elias Roberts <elias@eroberts.dev>
2020-09-06 14:21:49 -0600

    moved carousel effect into separate function

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,6 +22,27 @@ const createImgList = (arr) => {
 	return list;
 };
 
+const imageCarouselEffect = () => {
+	let carousel = document.querySelectorAll('.carousel');
+	for (let post of carousel) {
+		let flkty = new Flickity(post, {
+			imagesLoaded: true,
+			percentPosition: false
+		});
+		let imgs = post.querySelectorAll('.carousel-cell img');
+		let docStyle = document.documentElement.style;
+		let transformProp = typeof docStyle.transform == 'string' ? 'transform' : 'WebkitTransform';
+
+		flkty.on('scroll', function() {
+			flkty.slides.forEach(function(slide, i) {
+				let img = imgs[i];
+				let x = (slide.target + flkty.x) * -1 / 3;
+				img.style[transformProp] = 'translateX(' + x + 'px)';
+			});
+		});
+	}
+};
+
 const renderGallery = async (arr) => {
 	// get data from Firestore
 	const posts = await db.collection('posts').get().then((snapshot) => {
@@ -62,26 +83,7 @@ const renderGallery = async (arr) => {
 
 		gallery.appendChild(card);
 	});
-
-	// Image Carousel Effect
-	let carousel = document.querySelectorAll('.carousel');
-	for (let post of carousel) {
-		let flkty = new Flickity(post, {
-			imagesLoaded: true,
-			percentPosition: false
-		});
-		let imgs = post.querySelectorAll('.carousel-cell img');
-		let docStyle = document.documentElement.style;
-		let transformProp = typeof docStyle.transform == 'string' ? 'transform' : 'WebkitTransform';
-
-		flkty.on('scroll', function() {
-			flkty.slides.forEach(function(slide, i) {
-				let img = imgs[i];
-				let x = (slide.target + flkty.x) * -1 / 3;
-				img.style[transformProp] = 'translateX(' + x + 'px)';
-			});
-		});
-	}
+	imageCarouselEffect();
 };
 
 renderGallery();
